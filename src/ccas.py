9ffb539b25e3b1ac36ce1b4c3818940f490aaba8
johnko <git@johnko.ca>
2015-10-30 00:58:30 -0400

    remove os.sep

--- src/ccas.py
+++ src/ccas.py
@@ -208,7 +208,7 @@ class CcasMaster(GFSMaster):
         # chunkuuids = self.read_manifest(filename)
         iso = time.strftime('%Y%m%dT%H%M%SZ')
         timestamp = repr(time.time())
-        deleted_filename = os.path.join( os.sep, 'hidden', 'deleted', iso, timestamp, filename)
+        deleted_filename = os.path.join( 'hidden', 'deleted', iso, timestamp, filename)
         # self.write_manifest(deleted_filename, chunkuuids)
         self.rename(filename, deleted_filename)
         if self.debug > 0: print "deleted file: " + filename + " renamed to " + \
